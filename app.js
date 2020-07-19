b49062014bb8cd72e31402f6d53fd80d59e847ae
Mario Casado <cmmario94@gmail.com>
2020-07-19 14:16:42 +0200

    new: added enpoint for telegram bot

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var usersRouter = require('./routes/getproviders');
+var getproviders = require('./routes/getproviders');
 
 var app = express();
 
@@ -12,6 +12,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/getproviders', usersRouter);
+app.use('/getproviders', getproviders);
+app.post(`/${process.env['TELEGRAM_TOKEN']}` , (req, res) => {
+    res.redirect(`/getproviders?q=${encodeURIComponent(req.body.message)}`)
+})
 
 module.exports = app;
